752db12f7cb0d69c3097e7dface90e7a0836bdaf
Tobias <sachstobia@aol.com>
2026-01-23 01:01:30 +0100

    delete sound.py

--- sound.py
+++
@@ -1,7 +0,0 @@
-sound.py - VanMoof S5/A5 BLE sound/feedback commands
-## This file has been merged into commands/sound.py and is now deprecated.
-    await asyncio.sleep(0.5)
-    for _ in range(3):
-        resp = await client.recv(0.5)
-        if not resp:
-            break
